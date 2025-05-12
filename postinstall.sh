bfefd78e4cc4b3662ae741e703eebf407bbb2905
servalabs <166895751+servalabs@users.noreply.github.com>
2025-05-12 14:41:18 +0530

    modified:   postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -290,7 +290,7 @@ ClientAliveCountMax 2
 Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com
 MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com
 EOF
-    }
+    fi
     
     # Configure networkadmin user
     log_info "Configuring networkadmin user..."
